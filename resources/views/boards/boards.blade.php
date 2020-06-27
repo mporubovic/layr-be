b32e50deecf38d9f052e7fb8bca0394368862d94
Matej Porubovic <mawej1@gmail.com>
2020-06-27 23:34:15 +0200

    first commit

---
+++ resources/views/boards/boards.blade.php
@@ -0,0 +1,16 @@
+<h2>User boards</h2>
+
+@foreach ($boards as $board)
+
+    <h3><a href="/boards/{{ $board->id}}">{{ $board->title }}</a></h3>
+
+@endforeach
+
+<h2>Create a new board</h2>
+<form method="POST" action="/boards">
+    @csrf <!-- Cross site request forgery -->
+
+    <label class="label" for="title">Title </label>
+    <input class="input" type="text" name="title" id="title">
+    <button class="button" type="submit">Create</button>
+</form>
---
+++ resources/views/boards/show.blade.php
@@ -0,0 +1,5 @@
+ID: {{ $board->id }} </br>
+Title: {{ $board->title }} </br>
+Owner: {{ $board->owner }} </br>
+Created at: {{ $board->created_at }} </br>
+Updated at: {{ $board->updated_at }}
