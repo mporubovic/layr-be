ceddc8f52bb78fc0e42f353b4d9a73dac3d403cd
Matej Porubovic <mawej1@gmail.com>
2020-11-29 20:14:24 +0100

    route any to index.html

--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,6 @@ use Illuminate\Support\Facades\File;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// Route::get('/apitest', 'BoardController@index');
-
-
-// Route::get('/', 'BoardController@index');
-
-
-// Route::group(['domain' => ''])
-
-// Route::get('/{any}', function () {
-//     return \File::get(public_path() . '/indexv.html');
-// })->where('any', '.*');
-
-
-// Route::domain('{subdomain}.' . env('APP_DOMAIN'))->group(function ($subdomain) {
-//     Route::get('/', function () {
-//         return File::get(public_path() . '/indexv.html');
-
-//     });
-// });
-
 Route::get('/', function () {
     return File::get(public_path() . '/index.html');
 });
@@ -46,20 +26,6 @@ Route::group(['prefix' => 'auth'], function () {
     Route::post('/register', 'Auth\RegisterController@register');
 });
 
-// Route::get('/boards', 'BoardController@index')->name('boards.index');
-// Route::post('/boards', 'BoardController@store');
-
-// // Route::get('/boards/{board}', 'BoardController@show')->name('boards.show');
-
-
-// Route::get('/boards/{board}/edit', 'BoardController@edit');
-// Route::put('/boards/{board}', 'BoardController@update');
-
-
-// Auth::routes();
-
-// // Route::post('/login', 'Auth\LoginController@login');
-// // Route::post('/logout', 'Auth\LoginController@logout');
-// // Route::post('/register', 'Auth\RegisterController@register');
-
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/{any}', function () {
+    return File::get(public_path() . '/index.html');
+})->where('any', '.*');
